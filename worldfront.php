58fbbdb02855d5d2bc8f282a2ae7a71416c5de24
m-dabas <dabas1994@gmail.com>
2018-02-25 06:10:36 -0600

    test

--- worldfront.php
+++ worldfront.php
@@ -8,10 +8,10 @@ $year = "2018";
 $sql = "SELECT * from Events WHERE YEAR(date) = $year";
 $result = mysqli_query($conn, $sql);
 while($row = mysqli_fetch_assoc($result)){
-    $results[] = $row;
+    $results[] = json_encode($row);
 }
 
-echo json_encode($results[34]);
+echo json_encode($results);
 /*echo "<ul id='current_cats'>";
 while($row = mysqli_fetch_assoc($result)) {
     echo "<li>{$row['tags']}</li>";
