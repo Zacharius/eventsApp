5f435d365bac35faea4eb0743a26a3cfcfad9606
m-dabas <dabas1994@gmail.com>
2018-02-25 06:05:20 -0600

    test

--- worldfront.php
+++ worldfront.php
@@ -3,12 +3,15 @@ These are all the categories currently in the database:
 include('database.php');
 $conn = connect_db();
 
+$results = array();
 $year = "2018";
 $sql = "SELECT * from Events WHERE YEAR(date) = $year";
 $result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
+while($row = mysqli_fetch_assoc($result)){
+    $results[] = $row;
+}
 
-echo json_encode($row);
+echo json_encode($results);
 /*echo "<ul id='current_cats'>";
 while($row = mysqli_fetch_assoc($result)) {
     echo "<li>{$row['tags']}</li>";
