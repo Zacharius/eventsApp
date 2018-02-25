72661d7d3c7eafa66020b8eab7de54cef9dd8f88
Zach Faddis <zach.faddis@gmail.com>
2018-02-25 01:00:36 -0600

    Better route.php

--- route.php
+++ route.php
@@ -8,9 +8,8 @@
 
 <?php
 
-$return = array();
 $origin = '8801 Cinnamon Creek dr, San Antonio, TX, 78240';
-$destination = '8233 Potranco Road, San Antonio, Tx, 78252';
+$destination = '8233 Potranco Road';
 $apiKey='AIzaSyAHkOp4TdEBDVIV6H9192SghZ-h4idguAU';
 
 function urlBuilder($origin, $destination, $apiKey)
@@ -25,7 +24,7 @@ function urlBuilder($origin, $destination, $apiKey)
 
     return $final;
 
-}
+        }
 
 $request = urlBuilder($origin,$destination,$apiKey);
 
@@ -38,50 +37,58 @@ $numHops=0;
 $json = json_decode($jsontmp, TRUE);
 foreach($json['routes'][0] as $key=>$val)//first route associative array
 {
-    if($key == 'legs')
+  if($key == 'legs')
+  {
+    foreach($val as $val2)//iterate through leg array
     {
-        foreach($val as $val2)//iterate through leg array
+      $numHops++;
+      foreach($val2 as $key3=>$val3)//associative array in each leg array member
+      {
+        if($key3 == 'duration')
         {
-            $numHops++;
-            foreach($val2 as $key3=>$val3)//associative array in each leg array member
-            {
-                if($key3 == 'duration')
-                {
-                    $length += $val3['value'];
-                    /*echo $length;
-                    echo "<br>";*/
-                    $return['length'] = $length;
-                }
-
-                if($key3 == 'distance')
-                {
-                    $distance += $val3['value'];
-                    /*echo $distance;
-                    echo "<br>";*/
-                    $return['distance'] = $distance;
-                }
-            }
+          $length += $val3['value'];
+          echo $length;
+          echo "<br>";
         }
+
+        if($key3 == 'distance')
+        {
+          $distance += $val3['value'];
+          echo $distance;
+          echo "<br>";
+        }
+      }
     }
-    if($key == 'fare')
-    {
-        $fare = $val['text'];
-        /*echo $fare;
-        echo "<br>";*/
-        $return['fare'] = $fare;
-    }
+  }
+  if($key == 'fare')
+  {
+    $fare = $val['text'];
+    echo $fare;
+    echo "<br>";
+  }
 }
 
+echo "echo $numHops";
+echo "<br>";
+
+$mile = $distance/1609.34;
+$hours = floor($length / 3600);
+$minutes =  ($length / 60) - ($hours * 60); 
+
+printf("Final time : %d:%d\n", $hours, $minutes); 
+echo "<br>";
+
+printf("Final Length : %.2f miles", $mile);
+echo "<br>";
 
 $mapURLBase="https://www.google.com/maps/dir/?api=1&travelmode=transit&";
 $origin = str_replace(' ','+', $origin);
 $destination = str_replace(' ','+', $destination);
 
 
-//echo "$mapURLBase&orgin=$origin&destination=$destination";
-$return['mapURLparams'] = "$mapURLBase&orgin=$origin&destination=$destination";
+echo "$mapURLBase&orgin=$origin&destination=$destination";
 
-echo json_encode($return);
+
 
 ?>
 </body>
