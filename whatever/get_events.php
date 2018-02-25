9f1cb5e5cca976b1271bbfca1b8d1b9b275d641c
m-dabas <dabas1994@gmail.com>
2018-02-25 06:42:02 -0600

    test

--- whatever/get_events.php
+++ whatever/get_events.php
@@ -13,7 +13,8 @@ if (empty($month)) { die("Month is blank."); }
 if (empty($year)) { die("Year is blank."); }
 
 // fetch names of events with the specified category
-$sql = "SELECT * from Events WHERE YEAR(date) = $year AND MONTH(date) = $month";
+//$sql = "SELECT * from Events WHERE YEAR(date) = $year AND MONTH(date) = $month";
+$sql = "select * from Events";
 if($result = mysqli_query($conn, $sql)){
     while($row = mysqli_fetch_assoc($result)){
         $results[] = $row;
