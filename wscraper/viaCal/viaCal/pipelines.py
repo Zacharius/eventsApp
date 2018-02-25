f68ca681cf1b23a05fae234cc41e1aaf05a1cac7
EC2 Default User <ec2-user@ip-172-31-21-102.us-east-2.compute.internal>
2018-02-25 08:04:30 +0000

    scrappy scrip

--- wscraper/viaCal/viaCal/pipelines.py
+++ wscraper/viaCal/viaCal/pipelines.py
@@ -7,7 +7,8 @@
 import os
 import sys
 import hashlib
-import MYSQLdb
+import mysql.connector 
+from mysql.connector import errorcode 
 #from scrapy.extensions import DropItem
 from scrapy.http import Request
 
@@ -16,31 +17,46 @@ from scrapy.http import Request
 
 class ViacalPipeline(object):
     def __init__(self):
-        conn = mysqldb.connector.connect(host=os.environ['RDS_HOSTNAME'],
-            database=os.environ['RDS_DB_NAME'],
+        conn = mysql.connector.connect(host=os.environ['RDS_HOSTNAME'],
+            database='viaSocial',
             user=os.environ['RDS_USERNAME'],
             password=os.environ['RDS_PASSWORD'])
 
 
 
     def process_item(self, item, spider):
+        
         try:
-            self.cursor("""INSERT INTO Events (date, ename, hname, description, address, cost, startTime, tags, dow)
-                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                            (item['date'].encode('utf-8')), 
-                            (item['title'].encode('utf-8')),
-                            (item['venue'].encode('utf-8')),
-                            (item['des'].encode('utf-8')),
-                            (item['address'].encode('utf-8')), 
-                            (item['cost'].encode('utf-8')),
-                            (item['time'].encode('utf-8')),
-                            (item['cats'].encode('utf-8')),
-                            (item['dow'].encode('utf-8')))
+            conn = mysql.connector.connect(host=os.environ['RDS_HOSTNAME'],
+            database='viaSocial',
+            user=os.environ['RDS_USERNAME'],
+            password=os.environ['RDS_PASSWORD'])
 
-            self.conn.commit()
 
-        except MYSQLdb.Error, e:
-            self.conn.commit()
-            print "MYSQL Error %d: %s" % (e.args[0], e.args[1])
+            cursor = conn.cursor()
+            insertEvent = ("INSERT INTO Events (date, ename, hname, description, address, cost, " 
+                            "startTime) "
+                            "VALUES (%s, %s, %s, %s, %s, %s, %s)" )
+
+            data = ((item['date'][0].encode('utf-8')), 
+                    (item['title'][0].encode('utf-8')),
+                    (item['venue'][0].encode('utf-8')),
+                    (item['des'][0].encode('utf-8')),
+                    (item['address'][0].encode('utf-8')), 
+                    (item['cost'][0].encode('utf-8')),
+                    (item['time'][0].encode('utf-8')))
+
+            cursor.execute(insertEvent, data)
+            conn.commit()
+            cursor.close()
+            conn.close()
+
+        except mysql.connector.Error as  e:
+            print "MYSQL Error %s", e
+            conn.close()
+
 
         return item
+
+    def spider_close(self, spider):
+       conn.close()
