167aba6e965039c9de7ea86e9b5d17e1864819e6
m-dabas <dabas1994@gmail.com>
2018-02-25 04:00:45 -0600

    check res

--- whatever/get_events.php
+++ whatever/get_events.php
@@ -5,7 +5,7 @@ $conn = connect_db();
 
 $results = array();
 $month = date("n",strtotime($_POST['month']));
-$year = str_replace('\"', '', $_POST['year']);
+$year = $_POST['year'];
 
 
 // data validation
@@ -13,7 +13,7 @@ if (empty($month)) { die("Month is blank."); }
 if (empty($year)) { die("Year is blank."); }
 
 // fetch names of events with the specified category
-$sql = "SELECT * from Events WHERE YEAR(date) = $year AND MONTH(date) = $month";
+$sql = "SELECT * from Events WHERE YEAR(date) = $year AND MONTH(date) = $month AND ename like 'AAA%'";
 if($result = mysqli_query($conn, $sql)){
     while($row = mysqli_fetch_assoc($result)){
         $results[] = $row;
