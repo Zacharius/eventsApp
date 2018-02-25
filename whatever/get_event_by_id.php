48967066d361c757755e77e97dc1e5eb22074860
m-dabas <dabas1994@gmail.com>
2018-02-25 02:52:25 -0600

    fix errs

--- whatever/get_event_by_id.php
+++ whatever/get_event_by_id.php
@@ -3,7 +3,6 @@
 include('../database.php');
 $conn = connect_db();
 
-$month = date("n",strtotime($_POST['month']));
 $id = $_POST['id'];
 
 // data validation
@@ -12,6 +11,6 @@ if (empty($id)) { die("id is blank"); }
 // fetch names of events with the specified category
 $sql = "SELECT * from Events WHERE id = $id";
 if($result = mysqli_query($conn, $sql)){
-    $row = mysqli_fetch_assoc($result));
+    $row = mysqli_fetch_assoc($result);
     echo(json_encode($row));
 }
