5d6eea8142b1ef8a150c7aec0723ed47f0c19866
Z <z@debian.home.net>
2018-04-03 22:28:22 -0500

    Search is working! you must begin use the url /search/<searchParam> for it to work, see urls.py

--- eventsApp/cal/views.py
+++ eventsApp/cal/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Event
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 class EventListView(generic.ListView):
-    model = Event
-
+   model = Event 
+    
 class EventDetailView(generic.DetailView):
     model = Event
 
+class searchView(generic.ListView):
+    template_name = 'cal/event_list.html'
+    
+    def get_queryset(self):
+        searchParam = self.kwargs['search']
+        return Event.objects.filter(Q(title__icontains=searchParam) |
+                                    Q(desc__icontains=searchParam))
 
