3374ea4681d821d05b472d54f2d1a507e454c7b7
m-dabas <dabas1994@gmail.com>
2018-04-14 18:57:47 -0500

    added working navbar search; added date filter placeholder

--- eventsApp/eventsApp/settings.py
+++ eventsApp/eventsApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1c1uy9=xg8*r3@l58zy5wjhxkmj_&td973=xg751c_#_bc3euv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eventnear.us']
+ALLOWED_HOSTS = ['localhost', '192.168.1.31']
 
 
 # Application definition
