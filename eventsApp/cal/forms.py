5e8051b309b476e4eb1f0d5692e3617c8d589e7e
b1khat <b2khat@gmail.com>
2018-05-31 19:16:41 -0500

    MOSTAFA fooder

--- eventsApp/cal/forms.py
+++ eventsApp/cal/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.core.exceptions import ValidationError
